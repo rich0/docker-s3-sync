29502c78f996fd030abdc9f939dc883e7cbec7df
root <root@14.www.fdp.oma.srv.firespring.com>
2016-11-18 08:19:44 -0600

    Fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+echo "CALLED WITH [ $* ]"
 while [ true ]
 do
-  $*
+  echo "RUNNING [ $* ]"
+  sh -c "$*"
 
   if [ "${SYNC_PERIOD:-0}" -le 0 ]
   then
+    echo "Sync period is <= 0, exiting"
     break
   fi
 
