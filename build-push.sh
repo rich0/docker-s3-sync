d42d15476d6890e04b1bdb0c063603bad09d8570
Richard Freeman <rich0@gentoo.org>
2024-04-25 11:26:18 -0400

    Disable cache.
    Signed-off-by: Richard Freeman <rich0@gentoo.org>

--- build-push.sh
+++ build-push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#docker build . --pull --no-cache --tag ghcr.io/rich0/docker-s3-sync:latest && docker push ghcr.io/rich0/docker-s3-sync:latest
-docker build . --pull --tag ghcr.io/rich0/docker-s3-sync:latest && docker push ghcr.io/rich0/docker-s3-sync:latest
+docker build . --pull --no-cache --tag ghcr.io/rich0/docker-s3-sync:latest && docker push ghcr.io/rich0/docker-s3-sync:latest
 
 
 
