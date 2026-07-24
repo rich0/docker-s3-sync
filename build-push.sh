a1a4f25edc198d4b570502ee72d3056b35156c13
Richard Freeman <rich@rich0.org>
2026-07-24 17:36:39 -0400

    tag previous image
    Signed-off-by: Richard Freeman <rich@rich0.org>

--- build-push.sh
+++ build-push.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 IMAGE=registry.rich0.org/public/docker-s3-sync
 
+regctl image copy  "${IMAGE}:latest" "${IMAGE}:previous"
 docker build . --pull --no-cache --tag "${IMAGE}:latest"
 docker push "${IMAGE}:latest"
