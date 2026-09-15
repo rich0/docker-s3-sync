1cab8e600d75abb005740037fb692228a3df2332
Richard Freeman <rich@rich0.org>
2026-09-15 13:41:10 -0400

    add version tag
    Signed-off-by: Richard Freeman <rich@rich0.org>

--- build-push.sh
+++ build-push.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 
 IMAGE=registry.rich0.org/public/docker-s3-sync
+VERSION=1.0.0
 
-regctl image copy  "${IMAGE}:latest" "${IMAGE}:previous"
-docker build . --pull --no-cache --tag "${IMAGE}:latest"
+#regctl image copy  "${IMAGE}:latest" "${IMAGE}:previous"
+docker build . --pull --no-cache --tag "${IMAGE}:latest" --tag "${IMAGE}:${VERSION}"
 docker push "${IMAGE}:latest"
+docker push "${IMAGE}:${VERSION}"
